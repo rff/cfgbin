9e537b178fc56d0a35af859ff663cb8a9b1158a5
rff <rff@email.com>
2013-11-04 14:52:52 -0200

    Adding support for ASCII in term-palette.sh.
    term-palette.sh now has some more options for fancy decoration of the
    palette frame, including pure ASCII and none (aka, no frame). But the
    default is still the utf8 and it is dummy and do not check for output
    terminal capability to handle utf8 chars.

--- bin/term-palette.sh
+++ bin/term-palette.sh
@@ -5,18 +5,64 @@
 # └───┘ ┗━━━┛
 
 CHARS='  '
+TYPE='none'
+OPT=${1:-'-u'}
 
-echo '┌──────────────────┐'
-#echo '┏━━━━━━━━━━━━━━━━━━┓'
+[ ${OPT} == '-u' ] && TYPE='utf8'
+[ ${OPT} == '-U' ] && TYPE='utf8-thick'
+[ ${OPT} == '-a' ] && TYPE='ascii'
+[ ${OPT} == '-A' ] && TYPE='ascii-thick'
+[ ${OPT} == '-s' ] && TYPE='simple'
+[ ${OPT} == '-n' ] && TYPE='none'
+
+case $TYPE in
+    utf8 )
+           LINE_TOP='┌──────────────────┐\n'
+        LINE_VBEGIN='│ '
+                            LINE_VEND=' │\n'
+        LINE_BOTTON='└──────────────────┘\n'
+    ;;
+    utf8-thick )
+           LINE_TOP='┏━━━━━━━━━━━━━━━━━━┓\n'
+        LINE_VBEGIN='┃ '
+                            LINE_VEND=' ┃\n'
+        LINE_BOTTON='┗━━━━━━━━━━━━━━━━━━┛\n'
+    ;;
+    ascii )
+           LINE_TOP='+------------------+\n'
+        LINE_VBEGIN='| '
+                            LINE_VEND=' |\n'
+        LINE_BOTTON='+------------------+\n'
+    ;;
+    ascii-thick )
+           LINE_TOP='+==================+\n'
+        LINE_VBEGIN='| '
+                            LINE_VEND=' |\n'
+        LINE_BOTTON='+==================+\n'
+    ;;
+    simple )
+        LINE_TOP='\n'
+        LINE_VBEGIN=' '
+        LINE_VEND=' \n'
+        LINE_BOTTON='\n'
+    ;;
+    none ) ;&
+    * )
+        LINE_TOP=''
+        LINE_VBEGIN=''
+        LINE_VEND='\n'
+        LINE_BOTTON=''
+    ;;
+esac
+
+echo -en "$LINE_TOP"
 for f in 40 100 ; do
-    echo -n '│ '
-#    echo -n '┃ '
+    echo -en "$LINE_VBEGIN"
     for i in {0..7} ; do
         (( c = i + f ))
         echo -en "\e[${c}m${CHARS}\e[0m";
     done
-    echo ' │'
-#    echo ' ┃'
+    echo -en "$LINE_VEND" 
 done
-echo '└──────────────────┘'
-#echo '┗━━━━━━━━━━━━━━━━━━┛'
+echo -en "$LINE_BOTTON"
+
