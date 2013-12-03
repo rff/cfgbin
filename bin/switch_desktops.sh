0b4c4e66ae1ad95c265c2c7f86dedfbb9226a992
rff <rff@email.com>
2013-12-03 14:20:42 -0200

    Adding setdesktoplayout[.c] & switch_desktops.sh.
    setdesktoplayout.c is a small program to change layout of virtual
    desktops in a window-managed X session. With It we can set desktops in
    horizontal (default in openbox) and vertical lines (gnome shell) and in
    a matrix (unity).
    switch_desktops.sh is a script to swap all windows between two desktops.
    It have some configuration parameters but nothing fancy.

---
+++ bin/switch_desktops.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+[ "$#" -eq '0' ] && exit 0
+
+# get arguments.
+d1=$1
+d2=${2:-$(xdotool get_desktop)}
+
+# Get number of desktops.
+dn=$(xdotool get_num_desktops)
+
+# Test if the input are numbers.
+test "$d1" -eq "$d1" &>/dev/null || exit 1
+test "$d2" -eq "$d2" &>/dev/null || exit 1
+
+# Test bounds, valid desktop numbers.
+[ "$d1" -lt '0' ] && exit 1
+[ "$d2" -lt '0' ] && exit 1
+[ "$d1" -ge "$dn" ] && exit 1
+[ "$d2" -ge "$dn" ] && exit 1
+
+# Test if is the same desktop twice.
+[ "$d1" -eq "$d2" ] && exit 0
+
+# Take list of windows in each desk.
+l1="$(xdotool search --desktop $d1 '' 2>/dev/null)"
+l2="$(xdotool search --desktop $d2 '' 2>/dev/null)"
+
+#echo $d1 $l1
+#echo $d2 $l2
+
+for w in $l1 ; do
+	xdotool set_desktop_for_window $w $d2
+done
+for w in $l2 ; do
+	xdotool set_desktop_for_window $w $d1
+done
+
+exit 0
