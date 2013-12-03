b529b7602cfb5ffd5deb293c5d28db3049cd7414
rff <rff@email.com>
2013-12-03 15:15:14 -0200

    Update for fmt-sensors.py.
    Add option --invert in fmt-sensors.py that invert the column order.
    Also add support for fmt-sensors.py in padtec.conkyrc.

--- bin/fmt-sensors.py
+++ bin/fmt-sensors.py
@@ -12,6 +12,7 @@ opt = option()
 parser = argparse.ArgumentParser()
 parser.add_argument('-s', '--sep', type=int, default=1, help='Define columns space')
 parser.add_argument('-w', '--width', type=int, default=0, help='Max line width')
+parser.add_argument('-i', '--invert', action='store_true', help='Invert First and Second columns')
 parser.parse_args(namespace=opt)
 
 
@@ -34,23 +35,30 @@ for l in p.stdout:
 	l = l.rstrip('\n').replace('\t', ' ').partition(' (')[0].strip()
 
 	maxcollen[i] = len(l) if maxcollen[i] < len(l) else maxcollen[i]
+	#print('i:{} -- n:{} -- len:{}'.format(i,n,len(lines)))
 	if i == 0:
 		lines.append([l])
 	else:
-		if len(lines) >= n:
+		if len(lines) > n:
 			lines[n].append(l)
 		else:
 			lines.append([' ', l])
 	n += 1
 
-maxlen=0
-if opt.width < maxcollen[0] + maxcollen[1]:
-	maxlen = maxcollen[0] + opt.sep
-else:
-	maxlen = opt.width - maxcollen[1]
+i = 0 # first column to be printed
+j = 1 # second column to be printed
+if opt.invert:
+	i = 1
+	j = 0
 
-for i in lines:
-	i.append('')
-	f = '{0:{fill}} {1}'.format(i[0], i[1], fill=maxlen)
+maxlen=0
+if opt.width < maxcollen[i] + maxcollen[j]:
+	maxlen = maxcollen[i] + opt.sep
+else:
+	maxlen = opt.width - maxcollen[j]
+
+for l in lines:
+	l.append('')
+	f = '{0:{fill}}{1}'.format(l[i], l[j], fill=maxlen)
 	print(f)
 
