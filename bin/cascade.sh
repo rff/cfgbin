50a31fa5014454a5b69b15fabbabc94f3a635218
rff <rff@email.com>
2014-01-17 15:38:59 -0200

    Adding right align functionality to cascade.sh.
    In cascade.sh the new '-r' option organize windows aligned with the
    right border. It is useful when a window that has a smaller width comes
    after to one larger, so the right corner keeps aways visible, even when
    window focus of the larger one go above the it. See figure below:
    
    NORMAL (ALIGN LEFT)       ALIGN RIGHT
    +--------------+          +--------------+
    |              |          |              |
    |   +-------+  |          |         +-------+
    |   |       |  |          |         |       |
    |   |   +-----------+     |        +-----------+
    |   |   |           |     |        |           |

--- bin/cascade.sh
+++ bin/cascade.sh
@@ -3,22 +3,42 @@
 ### get active desktop ID.
 desktopID=$(xdotool get_desktop)
 
-xy=0
+xy=20
+x=$xy
+y=$xy
 step=20
+prev_w=0
+alignr='no'
+
+[ "$1" == '-r' ] && alignr='yes'
+
+
+
 
 ### list windows from all desktops including
 ### geometry information and sorted by Y position
 ### OBS: The list command is in the end of the while loop.
-
-while read winID deskID x y w h res; do
-	#$winID $deskID $x $y $w $h
-	[ $deskID -ne $desktopID ] && continue
+while read winID deskID winX winY winW winH winRES; do
+	[ "$deskID" -ne "$desktopID" ] && continue
+	if [ "$alignr" == 'yes' ] ; then
+		#(( x = prev_w > w ? xy + prev_w - w : xy ))
+		(( prev_w = prev_w == 0 ? winW : prev_w ))
+		(( x = x + prev_w - winW ))
+		(( x = x < 0 ? y : x ))
+		(( prev_w = winW ))
+	else
+		(( x = y ))
+	fi
+	wmctrl -iR $winID
+	wmctrl -ir $winID -e 0,$x,$y,-1,-1
 	#xdotool windowmove --sync $winID $xy $xy
-	wmctrl -iR $winID 
-	wmctrl -ir $winID -e 0,$xy,$xy,-1,-1
-	(( xy+=step )) 
-	printf "%s %2d %4d %4d %4d %4d $res\n" $winID $deskID $x $y $w $h
+	(( y += step ))
+	(( x += step ))
+	printf "%s %2d %4d %4d %4d %4d %s\n" $winID $deskID $winX $winY $winW $winH
 done < <(wmctrl -lG | sort -h -k 4)
+
+
+
 
 ### Old code I got from which I create mine code.
 #for windowId in $(xdotool search --desktop $desktopID "") ; do
