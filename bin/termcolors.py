a2710a72ab290e4739eb6e92e5925a775377aeeb
rff <rff@email.com>
2014-07-18 15:20:49 -0300

    Add termcolors.py script.
    This is my rewrite of terminalcolors.py. I think It is kinda good now.
    It automatically detect the terminal # of colors.

---
+++ bin/termcolors.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# Copyright (C) 2006 by Johannes Zellner, <johannes@zellner.org>
+# modified by mac@calmar.ws to fit my output needs
+# modified by rff@ to fit my output needs(2)
+
+import sys
+import os
+import argparse
+#import subprocess
+
+FGCOLOR=16 # black
+COLOROPTIONS = (8, 16, 88, 256)
+
+def nl():
+    os.system('echo')
+
+def out(bgcolor):
+    os.system('tput setaf {}'.format(FGCOLOR))
+    os.system('tput setab {}'.format(bgcolor))
+    os.system('echo -n "{:4}"'.format(bgcolor))
+    #os.system("tput setab 0")  # not working to restore to default background, it set to black.
+    os.system('/bin/echo -ne "\e[0m"')
+
+def printblock(start, end, width):
+    for y in range(start, end):
+        out(y)
+        if (y - start) % width == width - 1: nl()
+
+def gettermcolors():
+    return int(os.popen('tput colors').read().strip())
+
+def setoptsconstants(opt):
+	opt.START = hourtominutes(8)
+	opt.END   = hourtominutes(17)
+	opt.LUNCH = hourtominutes(1)
+	opt.HOURS = opt.END - opt.START - opt.LUNCH
+	opt.DRIFT = 9
+	opt.SHIFT = (opt.START, opt.END)
+### end setoptsCONSTANTS
+
+
+def main():
+	opt = parse()
+	setoptsconstants(opt)
+def parseargs():
+    termcolors = gettermcolors()
+    parser = argparse.ArgumentParser()
+    for c in COLOROPTIONS:
+        parser.add_argument('--{}'.format(c), action='store_const', const=c,
+                            default=termcolors, dest='termcolors',
+                            help='Force {} colors terminal test'.format(c))
+    parser.add_argument('-c', '--colors', action='store', choices=COLOROPTIONS,
+                        type=int, default=termcolors, dest='termcolors',
+                        help="Force N colors terminal test")
+    return parser.parse_args()
+
+def main():
+    opts = parseargs()
+
+    colors = opts.termcolors
+    cubewidth = 4 if colors == 88 else 6
+    graystart = 16 + cubewidth**3
+
+    print('{} colors terminal'.format(colors))
+
+    if colors < 8: return 0
+    print('ANSI 8 colors:')
+    printblock(0, 8, 8)
+    print
+
+    if colors < 16: return 0
+    print('High-intensity (bold/blink) of ANSI 8 colors:')
+    printblock(8, 16, 8)
+    print
+
+    if colors < 88: return 0
+    print('{0}x{0}x{0} RGB color cube:'.format(cubewidth))
+    printblock(16, graystart, cubewidth)
+    print
+
+    print('Grayscale ramp:')
+    printblock(graystart, colors, 8)
+    print
+
+
+
+if __name__ == '__main__': main()
+
