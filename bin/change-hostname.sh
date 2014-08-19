9acea9f7e8029553f69f3bf0917d56f3871e7bb2
rff <rff@email.com>
2014-08-19 08:36:36 -0300

    Small format and spell fixes.

--- bin/change-hostname.sh
+++ bin/change-hostname.sh
@@ -12,8 +12,8 @@ hostname_cmd=$(hostname)
 hostname ${hostname_new}                                             || exit $?
 echo ${hostname_new} > /etc/hostname                                 || exit $?
 
-# We try or best to find the hostname in the hosts that is the local host. We
-# do it without assumptions about the ip associated at it.
+# We try our best to find the hostname in the hosts that is the localhost. We
+# do it without assumptions about the ip associated with it.
 perl -p -i'.bkp' -e "s/${hostname_def}/${hostname_new}/" /etc/hosts  || exit $?
 perl -p -i'.bkp' -e "s/${hostname_var}/${hostname_new}/" /etc/hosts  || exit $?
 perl -p -i'.bkp' -e "s/${hostname_arq}/${hostname_new}/" /etc/hosts  || exit $?
