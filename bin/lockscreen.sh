6e80d5edc8e598c8fa1f8577426e1ee48e093934
RFF <rff@email.com>
2016-01-21 20:42:57 -0200

    lockscreen.sh: poweroff monitor when locking screen.
    Pretty much what the title says. Before locking the screen call dpms to
    turnoff the screen (so no backlight on while in a locked blackscreen).

--- bin/lockscreen.sh
+++ bin/lockscreen.sh
@@ -3,6 +3,7 @@ CHANNELS_ON="$(amixer get Master | grep -c '\[on\]')"
 
 [ "${CHANNELS_ON}" != "0" ] && amixer set Master mute   >/dev/null
 
+xset dpms force off
 slock
 
 [ "${CHANNELS_ON}" != "0" ] && amixer set Master unmute >/dev/null
