6ed27c57b4c9b48212d337bdeaba9d5db94c3f77
RFF <rff@email.com>
2017-02-13 12:49:30 -0200

    Bugfix: lockscreen.sh resume (enable) dpms when unlock
    Also add a small delay to lock, to avoid immediately brighten the screen
    when locking moving the mouse or pressing some key.

--- bin/lockscreen.sh
+++ bin/lockscreen.sh
@@ -3,7 +3,9 @@ CHANNELS_ON="$(amixer get Master | grep -c '\[on\]')"
 
 [ "${CHANNELS_ON}" != "0" ] && amixer set Master mute   >/dev/null
 
+sleep 1s
 xset dpms force off
 slock
+xset +dpms
 
 [ "${CHANNELS_ON}" != "0" ] && amixer set Master unmute >/dev/null
